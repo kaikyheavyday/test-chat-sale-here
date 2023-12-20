43795de0dceb3f472aff5cdc16bef9a608228d19
Pongpipat Tawatchaidumrong <pongpipat.t@knacx.com>
2023-12-20 22:30:14 +0700

    add user

--- chat-app/src/App.jsx
+++ chat-app/src/App.jsx
@@ -1,7 +1,32 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
+import { socket } from "./libs/socket";
+
+import Logo from "./assets/images/logo.png";
 function App() {
-  return <>test</>;
+  const [name, setName] = useState("");
+  const handleSubmitName = () => {
+    socket.emit("add:user", name);
+  };
+
+  return (
+    <div className="app">
+      <img src={Logo} alt="logo" className="logo" width={150} />
+      <div className="container">
+        <div className="form-name">
+          <h2 className="title">ชื่อของคุณ</h2>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+          {name && <button onClick={handleSubmitName}>ยืนยัน</button>}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
